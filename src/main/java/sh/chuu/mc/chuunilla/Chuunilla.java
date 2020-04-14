f7ae9e2310b13f43309027eb8dbbec51ce370ed7
Simon Chuu <simonorj@outlook.com>
2020-04-13 23:25:16 -0400

    Add Player heads to wandering trader + decrease chances of other rare trades

--- src/main/java/sh/chuu/mc/chuunilla/Chuunilla.java
+++ src/main/java/sh/chuu/mc/chuunilla/Chuunilla.java
@@ -1,7 +1,11 @@
 package sh.chuu.mc.chuunilla;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import sh.chuu.mc.chuunilla.listeners.*;
+
+import java.util.List;
 
 public class Chuunilla extends JavaPlugin {
     private static Chuunilla instance = null;
@@ -28,12 +32,44 @@ public class Chuunilla extends JavaPlugin {
         if (getConfig().getBoolean("beacon-mob-grief-protection", false))
             getServer().getPluginManager().registerEvents(new BeaconNoGrief(), this);
 
-        if (getConfig().getBoolean("wandering-trader-rare-trade", false))
+        if (getConfig().getBoolean("wandering-trader.rare-trade", false))
             getServer().getPluginManager().registerEvents(new WanderingTraderMod(), this);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length == 0 || args[0].equalsIgnoreCase("status")) {
+            sender.sendMessage("Modules: disable-thunder-fire: " +
+                    getConfig().getBoolean("disable-thunder-fire", false) + ", crop-auto-plant:" +
+                    getConfig().getBoolean("crop-auto-plant", false) + ", beacon-mob-grief-protection:" +
+                    getConfig().getBoolean("beacon-mob-grief-protection", false) + "; wandering-trader.rare-trade:" +
+                    getConfig().getBoolean("wandering-trader.rare-trade", false)
+            );
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("wanderinghead")) {
+            if (args.length == 1) {
+                sender.sendMessage("missing: <list|add|remove>");
+                return true;
+            }
+            List<String> ids = getConfig().getStringList("wandering-trader.heads");
+            if (args[1].equalsIgnoreCase("list")) {
+                // TODO
+            }
+            if (args[1].equalsIgnoreCase("remove")) {
+                // TODO
+            }
+            if (args[1].equalsIgnoreCase("add")) {
+                // TODO
+            }
+            sender.sendMessage("Head list config mod not yet implemented");
+            return true;
+        }
+        return true;
     }
 }
--- src/main/java/sh/chuu/mc/chuunilla/listeners/WanderingTraderMod.java
+++ src/main/java/sh/chuu/mc/chuunilla/listeners/WanderingTraderMod.java
@@ -1,32 +1,42 @@
 package sh.chuu.mc.chuunilla.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.WanderingTrader;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.meta.SkullMeta;
 import sh.chuu.mc.chuunilla.Chuunilla;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class WanderingTraderMod implements Listener {
+    private final Chuunilla plugin;
+    private final MerchantRecipe totem;
     private final MerchantRecipe hots;
     private final MerchantRecipe elytra;
     private final MerchantRecipe beacon;
 
     public WanderingTraderMod() {
+        this.plugin = Chuunilla.getInstance();
+        this.totem = new MerchantRecipe(new ItemStack(Material.TOTEM_OF_UNDYING, 1), 1);
+        this.totem.addIngredient(new ItemStack(Material.EMERALD, 57));
         this.hots = new MerchantRecipe(new ItemStack(Material.HEART_OF_THE_SEA, 1), 1);
         this.hots.addIngredient(new ItemStack(Material.EMERALD, 60));
+        this.hots.addIngredient(new ItemStack(Material.DIAMOND, 1));
         this.elytra = new MerchantRecipe(new ItemStack(Material.ELYTRA, 1), 1);
         this.elytra.addIngredient(new ItemStack(Material.EMERALD, 55));
+        this.elytra.addIngredient(new ItemStack(Material.PHANTOM_MEMBRANE, 2));
         this.beacon = new MerchantRecipe(new ItemStack(Material.BEACON, 1), 1);
         this.beacon.addIngredient(new ItemStack(Material.EMERALD, 52));
+        this.beacon.addIngredient(new ItemStack(Material.GLASS, 1));
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -35,13 +45,48 @@ public class WanderingTraderMod implements Listener {
             return;
 
         WanderingTrader trader = (WanderingTrader) ev.getEntity();
-        int rng = new Random().nextInt(3);
+        int rng = new Random().nextInt(7);
         List<MerchantRecipe> trades = new ArrayList<>(trader.getRecipes());
 
-        if (rng == 0) trades.add(hots);
-        else if (rng == 1) trades.add(elytra);
-        else trades.add(beacon);
+        if (rng == 0) trades.add(totem);
+        else if (rng == 1) trades.add(hots);
+        else if (rng == 2) trades.add(elytra);
+        else if (rng == 3) trades.add(beacon);
+        else {
+            trades.add(head(randomPlayer(null)));
+        }
+        trades.add(head(randomPlayer(ev.getLocation())));
 
         trader.setRecipes(trades);
+    }
+
+    private MerchantRecipe head(OfflinePlayer owner) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+
+        //noinspection ConstantConditions this is head and is never not null whatchu talking about
+        meta.setOwningPlayer(owner);
+        head.setItemMeta(meta);
+        MerchantRecipe a = new MerchantRecipe(head, owner.isOnline() ? 2 : 1);
+        a.addIngredient(new ItemStack(Material.EMERALD, owner.isOnline() ? 48 : 56));
+        return a;
+    }
+
+    private OfflinePlayer randomPlayer(Location near) {
+        if (near != null) {
+            OfflinePlayer ret = null;
+            double dist = Double.MAX_VALUE;
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                double d = near.distanceSquared(p.getLocation());
+                if (dist > d) {
+                    ret = p;
+                    dist = d;
+                }
+            }
+            return ret;
+        }
+
+        List<String> heads = plugin.getConfig().getStringList("wandering-trader.heads");
+        return Bukkit.getOfflinePlayer(UUID.fromString(heads.get(new Random().nextInt(heads.size()))));
     }
 }
