fe73324e89d8cc282f30127a955828043860e5be
Simon Chuu <simonorj@outlook.com>
2020-05-27 16:21:49 -0400

    WanderingTraderMod: Decrease rare item chances + increase rare item prices + add dead bush, totem of undying, and trident to the random pool

--- src/main/java/sh/chuu/mc/chuunilla/listeners/WanderingTraderMod.java
+++ src/main/java/sh/chuu/mc/chuunilla/listeners/WanderingTraderMod.java
@@ -28,21 +28,22 @@ public class WanderingTraderMod implements Listener {
 
     public WanderingTraderMod() {
         this.plugin = Chuunilla.getInstance();
-        this.bush = new MerchantRecipe(new ItemStack(Material.DEAD_BUSH, 2), 2);
+        this.bush = new MerchantRecipe(new ItemStack(Material.DEAD_BUSH, 10), 2);
         this.bush.addIngredient(new ItemStack(Material.EMERALD, 3));
         this.totem = new MerchantRecipe(new ItemStack(Material.TOTEM_OF_UNDYING, 1), 1);
         this.totem.addIngredient(new ItemStack(Material.EMERALD, 57));
         this.hots = new MerchantRecipe(new ItemStack(Material.HEART_OF_THE_SEA, 1), 1);
-        this.hots.addIngredient(new ItemStack(Material.EMERALD, 60));
-        this.hots.addIngredient(new ItemStack(Material.DIAMOND, 1));
+        this.hots.addIngredient(new ItemStack(Material.EMERALD_BLOCK, 12));
+        this.hots.addIngredient(new ItemStack(Material.NAUTILUS_SHELL, 12));
         this.elytra = new MerchantRecipe(new ItemStack(Material.ELYTRA, 1), 1);
-        this.elytra.addIngredient(new ItemStack(Material.EMERALD, 55));
-        this.elytra.addIngredient(new ItemStack(Material.PHANTOM_MEMBRANE, 2));
+        this.elytra.addIngredient(new ItemStack(Material.EMERALD_BLOCK, 12));
+        this.elytra.addIngredient(new ItemStack(Material.PHANTOM_MEMBRANE, 42));
         this.beacon = new MerchantRecipe(new ItemStack(Material.BEACON, 1), 1);
-        this.beacon.addIngredient(new ItemStack(Material.EMERALD, 52));
-        this.beacon.addIngredient(new ItemStack(Material.GLASS, 1));
+        this.beacon.addIngredient(new ItemStack(Material.EMERALD_BLOCK, 12));
+        this.beacon.addIngredient(new ItemStack(Material.OBSIDIAN, 56));
         this.trident = new MerchantRecipe(new ItemStack(Material.TRIDENT, 1), 1);
-        this.trident.addIngredient(new ItemStack(Material.EMERALD, 62));
+        this.trident.addIngredient(new ItemStack(Material.EMERALD_BLOCK, 12));
+        this.trident.addIngredient(new ItemStack(Material.DIAMOND, 32));
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -51,50 +52,16 @@ public class WanderingTraderMod implements Listener {
             return;
 
         WanderingTrader trader = (WanderingTrader) ev.getEntity();
-        int rng = new Random().nextInt(10);
+        int rng = new Random().nextInt(8);
         List<MerchantRecipe> trades = new ArrayList<>(trader.getRecipes());
 
         if (rng == 0) trades.add(trident);
         else if (rng == 1) trades.add(hots);
         else if (rng == 2) trades.add(elytra);
         else if (rng == 3) trades.add(beacon);
-        else if (rng <= 5) trades.add(bush);
-        else if (rng <= 7) trades.add(totem);
-        else {
-            trades.add(head(randomPlayer(null)));
-        }
-        trades.add(head(randomPlayer(ev.getLocation())));
+        else if (rng <= 5) trades.add(totem);
+        else trades.add(bush);
 
         trader.setRecipes(trades);
-    }
-
-    private MerchantRecipe head(OfflinePlayer owner) {
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1);
-        SkullMeta meta = (SkullMeta) head.getItemMeta();
-
-        //noinspection ConstantConditions this is head and is never not null whatchu talking about
-        meta.setOwningPlayer(owner);
-        head.setItemMeta(meta);
-        MerchantRecipe a = new MerchantRecipe(head, owner.isOnline() ? 2 : 1);
-        a.addIngredient(new ItemStack(Material.EMERALD, owner.isOnline() ? 48 : 56));
-        return a;
-    }
-
-    private OfflinePlayer randomPlayer(Location near) {
-        if (near != null) {
-            OfflinePlayer ret = null;
-            double dist = Double.MAX_VALUE;
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                double d = near.distanceSquared(p.getLocation());
-                if (dist > d) {
-                    ret = p;
-                    dist = d;
-                }
-            }
-            return ret;
-        }
-
-        List<String> heads = plugin.getConfig().getStringList("wandering-trader.heads");
-        return Bukkit.getOfflinePlayer(UUID.fromString(heads.get(new Random().nextInt(heads.size()))));
     }
 }
