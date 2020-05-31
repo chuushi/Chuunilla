37ddd5cff844c5d8e8df90cc532c5a6b1530ffa7
Simon Chuu <simonorj@outlook.com>
2020-05-30 23:17:41 -0400

    I rewrote Timber again qwq, incomplete
    it almost works.
    
    - Another branch on top are not caught.
    - Leaf detection doesn't work yet.

--- src/main/java/sh/chuu/mc/chuunilla/listeners/Timber.java
+++ src/main/java/sh/chuu/mc/chuunilla/listeners/Timber.java
@@ -15,19 +15,11 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import sh.chuu.mc.chuunilla.Chuunilla;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 // Usage: Strip log first, then break the stripped log to active timber
 public class Timber implements Listener {
     private static final String PERMISSION_NODE = "chuunilla.timber";
-    private enum Specie {
-        OAK, SPRUCE, BIRCH, JUNGLE, DARK_OAK, ACACIA
-    }
-    private enum CheckResult {
-        LOG, NO_LEAVES, NONE
-    }
 
     private final Chuunilla plugin = Chuunilla.getInstance();
 
@@ -56,13 +48,12 @@ public class Timber implements Listener {
             return;
 
         p.sendMessage("Timber Activated");
-        List<Block> blocks = new ArrayList<>();
+        TreeCheck c = new TreeCheck(b);
 
-        if (populateLogList(blocks, 0, getSpecie(b.getType()), b, BlockFace.SELF)) {
+        if (c.checkSurrounding(b, BlockFace.SELF, 0, true)) {
             p.sendMessage("Timber Passed");
-            unstripLog(b);
-
-            Iterator<Block> it = blocks.iterator();
+            c.unstrip();
+            Iterator<Block> it = c.logs.iterator();
 
             new BukkitRunnable() {
                 @Override
@@ -72,87 +63,9 @@ public class Timber implements Listener {
                 }
             }.runTaskTimer(plugin, 0L, interval);
         }
-        p.sendMessage("Timber Blocks: " + blocks.size());
+        p.sendMessage("Timber Blocks: " + c.logs.size());
     }
 
-    /*
-    Psuedobode: (itself is a log)
-    1. traverse up and check
-    2. if not log, check blocks surrounding up
-    3. check blocks surrounding itself
-     */
-    // TODO This is so broken
-    /**
-     Psuedobode: (itself is a log)
-     1. traverse up and check
-     2. if up is not same specie, check surrounding of up
-     3.   if nothing surrounds it, exit.
-     4. if log, check surrounding then recursive call to up
-     5. else if leaves, check surrounding then
-     6. else just return false because idk wtf happened
-     3. check blocks surrounding itself
-     * @param list List of blocks
-     * @param side How many blocks search went sideways
-     * @param specie Specie
-     * @param block Block to center the search around
-     * @param face Direction of block to check
-     * @return true if at least one branch was detected
-     */
-    private boolean populateLogList(List<Block> list, int side, Specie specie, Block block, BlockFace face) {
-        if (side >= 5) return false;
-
-        Block bl = block.getRelative(face);
-        list.add(bl);
-
-        // Get the above block to check
-        // if it's a log
-        // then recursive call
-        // else check its surroundings
-        Block up = bl.getRelative(BlockFace.UP);
-
-        if (specie != getSpecie(up.getType())) {
-            return checkSurrounding(list, side + 1, specie, bl, face) == CheckResult.LOG
-                    || checkSurrounding(list, 0, specie, up, BlockFace.UP) == CheckResult.LOG;
-        }
-
-        if (isLog(up)) {
-            if (list.contains(up))
-                return true;
-
-            return checkSurrounding(list, side+ + 1, specie, bl, face) != CheckResult.NO_LEAVES && populateLogList(list, 0, specie, bl, BlockFace.UP);
-        }
-
-        if (isNaturalLeaves(up)) {
-            return checkSurrounding(list, side + 1, specie, bl, face) != CheckResult.NO_LEAVES
-                    || checkSurrounding(list, 0, specie, up, BlockFace.UP) != CheckResult.NO_LEAVES;
-        }
-
-        return false;
-    }
-
-    /**
-     *
-     * @param list List of blocks
-     * @param side how many blocks search went sideways
-     * @param specie Specie
-     * @param bl Block to center the search around
-     * @param face Direction of block relative to previous block
-     * @return true if at least one branch was detected, false if no branch was detected, null if branch ends without leaves
-     */
-    private CheckResult checkSurrounding(List<Block> list, int side, Specie specie, Block bl, BlockFace face) {
-        CheckResult ret = CheckResult.NONE;
-        for (BlockFace f : adjacent) {
-            if (facePass(face, f)) continue;
-
-            Block b = bl.getRelative(f);
-            if (specie == getSpecie(b.getType()) && isLog(b) && !list.contains(b)) {
-                if (!populateLogList(list, side, specie, bl, f))
-                    return CheckResult.NO_LEAVES;
-                ret = CheckResult.LOG;
-            }
-        }
-        return ret;
-    }
 
     private boolean facePass(BlockFace face, BlockFace check) {
         int fx = face.getModX();
@@ -180,20 +93,6 @@ public class Timber implements Listener {
         }
     }
 
-    private boolean isLog(Block b) {
-        switch (b.getType()) {
-            case OAK_LOG:
-            case SPRUCE_LOG:
-            case BIRCH_LOG:
-            case JUNGLE_LOG:
-            case DARK_OAK_LOG:
-            case ACACIA_LOG:
-                return true;
-            default:
-                return false;
-        }
-    }
-
     private boolean isInitialLog(Block b) {
         BlockData data = b.getBlockData();
         // Check if initial log is orientated in Y (natural logs are usually like this)
@@ -213,62 +112,110 @@ public class Timber implements Listener {
         }
     }
 
-    private boolean isNaturalLeaves(Block b) {
-        BlockData data = b.getBlockData();
-        return data instanceof Leaves && !((Leaves) data).isPersistent();
-    }
+    private final class TreeCheck {
+        private final Set<Block> logs = new LinkedHashSet<>();
+        private final Block initial;
+        private final Material leaves;
+        private final Material log;
 
-    private Specie getSpecie(Material tree) {
-        switch (tree) {
-            case OAK_LOG:
-            case STRIPPED_OAK_LOG:
-            case OAK_LEAVES:
-                return Specie.OAK;
-            case SPRUCE_LOG:
-            case STRIPPED_SPRUCE_LOG:
-            case SPRUCE_LEAVES:
-                return Specie.SPRUCE;
-            case BIRCH_LOG:
-            case BIRCH_LEAVES:
-            case STRIPPED_BIRCH_LOG:
-                return Specie.BIRCH;
-            case JUNGLE_LOG:
-            case STRIPPED_JUNGLE_LOG:
-            case JUNGLE_LEAVES:
-                return Specie.JUNGLE;
-            case DARK_OAK_LOG:
-            case STRIPPED_DARK_OAK_LOG:
-            case DARK_OAK_LEAVES:
-                return Specie.DARK_OAK;
-            case ACACIA_LOG:
-            case STRIPPED_ACACIA_LOG:
-            case ACACIA_LEAVES:
-                return Specie.ACACIA;
-            default:
-                return null;
+        private TreeCheck(Block initialBlock) {
+            Material[] type = getLogLeaves(initialBlock.getType());
+            if (type != null) {
+                this.leaves = type[1];
+                this.log = type[0];
+                this.initial = initialBlock;
+                this.logs.add(initialBlock);
+            } else {
+                this.leaves = null;
+                this.log = null;
+                this.initial = null;
+            }
         }
-    }
 
-    private void unstripLog(Block b) {
-        switch (b.getType()) {
-            case STRIPPED_OAK_LOG:
-                b.setType(Material.OAK_LOG, false);
-                break;
-            case STRIPPED_SPRUCE_LOG:
-                b.setType(Material.SPRUCE_LOG, false);
-                break;
-            case STRIPPED_BIRCH_LOG:
-                b.setType(Material.BIRCH_LOG, false);
-                break;
-            case STRIPPED_JUNGLE_LOG:
-                b.setType(Material.JUNGLE_LOG, false);
-                break;
-            case STRIPPED_DARK_OAK_LOG:
-                b.setType(Material.DARK_OAK_LOG, false);
-                break;
-            case STRIPPED_ACACIA_LOG:
-                b.setType(Material.ACACIA_LOG, false);
-                break;
+        private boolean checkSurrounding(Block bl, BlockFace face, int side, boolean isLog) {
+            if (side > 5) return false;
+            int newSide = side + 1;
+            Map<Block, BlockFace> toCheck = new LinkedHashMap<>();
+
+            // itself
+            if (isLog)
+                toCheck.put(bl, face);
+            for (BlockFace f : adjacent) {
+                if (side != 0 && facePass(face, f)) continue;
+
+                Block b = bl.getRelative(f);
+                if (isLog(b) && addBlock(b)) {
+                    toCheck.put(b, f);
+                    checkSurrounding(b, f, newSide, true);
+                }
+            }
+
+            // Upwards
+            for (Map.Entry<Block, BlockFace> e : toCheck.entrySet()) {
+                Block up = e.getKey().getRelative(BlockFace.UP);
+                System.out.printf("y: %d\n", up.getY());
+
+                if (isLog(up)) {
+                    if (addBlock(up) && !checkSurrounding(up, e.getValue(), side, true))
+                        return false;
+                    continue;
+                }
+
+                if (checkSurrounding(up, BlockFace.SELF, side, false))
+                    return true;
+
+                if (!checkLeaves(up))
+                    return false;
+            }
+            return true;
         }
+
+
+        private void unstrip() {
+            initial.setType(log, false);
+        }
+
+        private boolean isLog(Block log) {
+            return log.getType() == this.log;
+        }
+
+        private boolean addBlock(Block log) {
+            return logs.add(log);
+        }
+
+        private boolean checkLeaves(Block leaves) {
+            return leaves.getType() == this.leaves && !((Leaves) leaves.getBlockData()).isPersistent();
+        }
+
+        private Material[] getLogLeaves(Material m) {
+            switch (m) {
+                case OAK_LOG:
+                case STRIPPED_OAK_LOG:
+                case OAK_LEAVES:
+                    return new Material[]{Material.OAK_LOG, Material.OAK_LEAVES};
+                case SPRUCE_LOG:
+                case STRIPPED_SPRUCE_LOG:
+                case SPRUCE_LEAVES:
+                    return new Material[]{Material.SPRUCE_LOG, Material.SPRUCE_LEAVES};
+                case BIRCH_LOG:
+                case BIRCH_LEAVES:
+                case STRIPPED_BIRCH_LOG:
+                    return new Material[]{Material.BIRCH_LOG, Material.BIRCH_LEAVES};
+                case JUNGLE_LOG:
+                case STRIPPED_JUNGLE_LOG:
+                case JUNGLE_LEAVES:
+                    return new Material[]{Material.JUNGLE_LOG, Material.JUNGLE_LEAVES};
+                case DARK_OAK_LOG:
+                case STRIPPED_DARK_OAK_LOG:
+                case DARK_OAK_LEAVES:
+                    return new Material[]{Material.DARK_OAK_LOG, Material.DARK_OAK_LEAVES};
+                case ACACIA_LOG:
+                case STRIPPED_ACACIA_LOG:
+                case ACACIA_LEAVES:
+                    return new Material[]{Material.ACACIA_LOG, Material.ACACIA_LEAVES};
+            }
+            return null;
+        }
+
     }
 }
